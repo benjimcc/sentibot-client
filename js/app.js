cd55f9f044049a290bb1ba1f40120642cb77d93e
Ben McCarthy <ben.mccarthy@autotrader.co.uk>
2014-12-15 14:27:50 +0000

    Added basic sentiment phrases

--- js/app.js
+++ js/app.js
@@ -15,4 +15,72 @@ angular.module('myApp', [
 	}
 	);
 
+		var tally = {};
+
+	var positiveColor = '#FF8586';
+	var negativeColor = '#63A69F';
+	var neutralColor = '#DECEB3';
+
+	var positive = {
+		type: 'positive',
+		icon: 'grinning-face.png'
+	};
+	var happy = {
+		type: 'positive',
+		icon: 'smiling-face.png'
+	};
+	var lovely = {
+		type: 'positive',
+		icon: 'heart-eyed-happy-face.png'
+	};
+	var negative = {
+		type: 'negative',
+		icon: 'pensive-face.png'
+	};
+	var sad = {
+		type: 'negative',
+		icon: 'crying-face.png'
+	};
+	var angry = {
+		type: 'negative',
+		icon: 'angry-face.png'
+	};
+	var sick = {
+		type: 'negative',
+		icon: 'sick-face.png'
+	};
+
+	var positiveWords = [
+		 'excellent', 'amazing', 'beautiful', 'nice', 'marvelous', 'magnificent', 'fabulous', 'astonishing', 'fantastic', 'peaceful', 'fortunate', 
+		 'brilliant', 'glorious', 'cheerful', 'gracious', 'grateful', 'splendid', 'superb', 'honorable', 'thankful', 'inspirational',
+		 'ecstatic', 'victorious', 'virtuous', 'proud', 'wonderful', 'lovely', 'delightful'
+	];
+	var happyWords = [
+		'happy', 'lucky', 'awesome', 'excited', 'fun', 'amusing', 'amused', 'pleasant', 'pleasing', 'glad', 'enjoy',
+		'jolly', 'delightful', 'joyful', 'joyous', ':-)', ':)', ':-D', ':D', '=)','☺'
+	];
+	var lovelyWords = [
+		'love', 'adore', 'blissful', 'heartfelt', 'loving', 'lovable', 'sweetheart', 'darling', 'kawaii', 'married', 'engaged'
+	];
+	var negativeWords = [
+		'unhappy', 'bad', 'sorry', 'annoyed', 'dislike', 'anxious', 'ashamed', 'cranky', 'crap', 'crappy', 'envy', 
+		'awful', 'bored', 'boring', 'bothersome', 'bummed', 'burned', 'chaotic', 'defeated', 'devastated', 'stressed',
+		'disconnected', 'discouraged', 'dishonest', 'doomed', 'dreadful', 'embarrassed', 'evicted', 'freaked out', 'frustrated', 'stupid',
+		'guilty', 'hopeless', 'horrible', 'horrified', 'humiliated', 'ignorant', 'inhumane', 'cruel', 'insane', 'insecure',
+		'nervous', 'offended', 'oppressed', 'overwhelmed', 'pathetic', 'powerless', 'poor', 'resentful', 'robbed', 'screwed'
+	];
+	var sadWords = [
+		'sad', 'alone', 'anxious', 'depressed', 'disappointed', 'disappointing', 'sigh', 'sobbing', 'crying', 'cried', 
+		'dumped', 'heartbroken', 'helpless', 'hurt', 'miserable', 'misunderstood', 'suicidal', ':-(', ':(', '=(', ';('
+	];
+	var angryWords = [
+		'hate', 'damn', 'angry', 'betrayed', 'bitched','disgust', 'disturbed', 'furious', 'harassed', 'hateful', 'hostile', 'insulted',
+		'irritable', 'jealous', ' rage ', 'pissed'
+
+	];
+	var sickWords = [
+		'sick', ' ill ', 'under weather', 'throw up', 'threw up', 'throwing up', 'puke', 'puking', 'pain', 'hangover', 'intoxicated'
+	];
+	
+
 }]);
